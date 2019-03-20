b09034bdf1055a5a93211e92cff4b018c9b52242
wuminghui <1032662429@qq.com>
2019-03-20 18:06:26 +0800

    WIP: Add some define

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status
+
 int main() {
    int *p = {1, 3};
    printf("%p", &p);
